8371c39eb78f069d8db9e6b2f2a7b0eeced0873f
Muhammadalif1213 <muhammadalif1213@gmail.com>
2023-04-07 09:41:35 +0700

    jawaban dari soal no 1-5 exercise

---
+++ Exercise1_algoritma&strukturdata/Exercise1_algoritma&strukturdata.cpp
@@ -0,0 +1,11 @@
+// jawabas soal exercise
+
+//1. algoritma sangat penting dalam menyelesaikan suatu masalah program karena algoritma merupakan langkah awal dalam mengurutkan suatu cara/ fungsi agar berjalan secara terstruktur.
+//2. dalam algoritma, struktur data di bagi menjadi 2 yaitu : 
+//		statis/ linear, contohnya array
+//		dinamis/non-linear, contohnya linked list
+//3. Faktor- faktor yang mempengaruhi efisiensi waktu algoritma yaitu:
+//		kecepatan mesin, complier, Operating system, Bahas pemrograman yang di gunakan, ukuran dari inputnya.
+//4. Berdasarkan materi yang telah di sampaikan, untuk penggunaan algoritma dalam jumlah data yang besar lebih mudah untuk menggunakan metode quicksort karena cara yang digunakan adalah mencari titik tengah (pivot) kemudian mencari mana yang terbesar dan yang terkecil dari titik tengah.
+//5. berdadsarkan algoritma yang telah di pelajari, algoritma yang termasuk quadratic adalah: Bubble sort, selection sort, Insertion sort
+//	 sedangkan yang loglinear adalah: mergesort dan quick sort.
