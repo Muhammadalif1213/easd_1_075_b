b43508120042faff1ad65561e271c2dfc0d28ca2
Muhammadalif1213 <muhammadalif1213@gmail.com>
2023-04-07 11:00:10 +0700

    fungsi mergesort

--- Exercise1_algoritma&strukturdata/Exercise1_algoritma&strukturdata.cpp
+++ Exercise1_algoritma&strukturdata/Exercise1_algoritma&strukturdata.cpp
@@ -9,3 +9,102 @@
 //4. Berdasarkan materi yang telah di sampaikan, untuk penggunaan algoritma dalam jumlah data yang besar lebih mudah untuk menggunakan metode quicksort karena cara yang digunakan adalah mencari titik tengah (pivot) kemudian mencari mana yang terbesar dan yang terkecil dari titik tengah.
 //5. berdadsarkan algoritma yang telah di pelajari, algoritma yang termasuk quadratic adalah: Bubble sort, selection sort, Insertion sort
 //	 sedangkan yang loglinear adalah: mergesort dan quick sort.
+
+//jawaban no 6
+
+#include <iostream>
+using namespace std;
+
+int alif[95];
+int n;
+int MA;
+int k;
+int i;
+
+void input() {
+	while (true)
+	{
+		cout << "masukan jumlah banyaknya elemen pada array:";
+		cin >> n;
+		if (n <= 95)
+			break;
+		else {
+			cout << "n\Maksimal array yaitu 95 elemen.\n";
+		}
+		cout << endl;								
+		cout << "===================" << endl;		
+		cout << "Masukan Elemen Array" << endl;	
+		cout << "===================" << endl;	
+
+		for (int i = 0; i < n; i++) {	
+			cout << "Data ke-" << (i + 1) << ":";	
+			cin >> n[i];					
+		}
+	}
+	
+}
+
+void merge(int alif[], int low, int high, int mid) {
+    int i, j, k;
+    int n1 = mid - low + 1;
+    int n2 = high - mid;
+
+    int L[n1], R[n2];
+
+    for (i = 0; i < n1; i++)
+        L[i] = arr[low + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    i = 0;
+    j = 0;
+    k = low;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            alif[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+
+void mergesort(int alif[], int low, int high) {
+	if (low >= high) {
+		return;
+	}
+	int mid = (low + high) / 2;
+
+}
+
+
+void output(int alif[], int n) {
+    cout << "array yang tersusun : ";
+    for (int i = 0; i < n; i++) {
+        cout << alif[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+		input();
+		mergesort();
+		output();
+	return 0;
+}
